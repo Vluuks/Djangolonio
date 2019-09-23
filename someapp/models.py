48c389fadcbc9290af2d70bd38324432f2ef0f11
Vluuks <vluuks@gmail.com>
2019-09-23 17:43:16 +0200

    admin access

--- someapp/models.py
+++ someapp/models.py
@@ -9,13 +9,6 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-class User(models.Model):
-    name = models.CharField(max_length=40)
-    register_date = models.DateTimeField(default=datetime.now, blank=True)
-
-    def __str__(self):
-        return f"{self.name}"
-
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
     description = models.CharField(max_length=1000)
