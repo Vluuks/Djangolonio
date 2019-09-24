01b49e1b5fee7d93ae9001832169db0ce89cd4ab
Vluuks <vluuks@gmail.com>
2019-09-24 20:29:38 +0200

    css and board page

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home", views.home_view, name="home")
+    path("home", views.home_view, name="home"),
+    path("boards/<int:board_id>/", views.board_view, name="board")
 ]
