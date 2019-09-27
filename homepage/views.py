10de48707673f0c8ef70f499a252823b5198d31a
Vluuks <vluuks@gmail.com>
2019-09-27 13:59:17 +0200

    somewhat more elegant home

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from pagelist import entries
 
 # Create your views here.
 def index(request):
-    return render(request, "homepage/index.html")
+    return render(request, "homepage/index.html", { "pages" : entries })
