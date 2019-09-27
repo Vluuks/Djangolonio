899e7752f42d06c6b80bcfa8d2189307bd762776
Vluuks <vluuks@gmail.com>
2019-09-27 11:29:19 +0200

    trying to make models

--- dndelements/models.py
+++ dndelements/models.py
@@ -33,26 +33,28 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=1000)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="races")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players"")
     age = models.IntegerField(default = 0)
     gender = models.CharField(max_length=40)
 
     # can only be one owner ?? i dont understand this one to many many to one thing anymore
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owners")
-    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, related_name="campaigns")
+    # so every time you inser t aplayer, you have to pick 1 owner, but you can do 2 times a player with the same owner
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="players")
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, related_name="players")
 
 
 class NPC(models.Model):
     name = models.CharField(max_length=40)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="races")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="npcs")
     age = models.IntegerField(default = 0)
     gender = models.CharField(max_length=40)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE, related_name="npcs")
 
 
 class Campaign(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
     description = models.CharField(max_length=1000)
     name = models.CharField(max_length=40)
+    users = models.ManyToMany(User, blank=True, related_name="campaigns")
     
     # locations = models.IntegerField(default = 0)
     # npcs = models.IntegerField(default = 0)
