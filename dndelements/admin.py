3763c433d99a076a6aa7ed3845ce1aff503d0358
Vluuks <vluuks@gmail.com>
2019-09-27 11:37:10 +0200

    admin page

--- dndelements/admin.py
+++ dndelements/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Player, Campaign, NPC, Race
+
+admin.site.register(Player)
+admin.site.register(Campaign)
+admin.site.register(NPC)
+admin.site.register(Race)
